4cbba6ed7b4ba12f9820a98b81d6a1c07dd79281
Pankaj <pgrover2079@gmail.com>
2019-12-11 16:11:06 -0800

    Functional & functional-shared complete

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -6,13 +6,48 @@ var Queue = function() {
 
   // Implement the methods below
 
+  // Helper function Max Value
+   const findMaxVal = function(input) {
+    let allKeys = Object.keys(input);
+
+    let maxVal = 0;
+
+    if(allKeys.length > 0) {
+      let allKeysNum = allKeys.map(function(ele) {
+        return Number(ele);
+      });
+      maxVal = Math.max.apply(Math, allKeysNum);
+    }
+
+    return maxVal;
+   }  
+
   someInstance.enqueue = function(value) {
+    let maxVal = findMaxVal(storage);
+    storage[maxVal+1] = value;
+    return (maxVal+1);
   };
 
   someInstance.dequeue = function() {
+    let maxVal = findMaxVal(storage);
+    
+    let returnVal;
+    if (maxVal > 0) {
+      returnVal = storage["1"];
+      delete storage["1"]
+      for(currKey in storage) {
+        let currKeyNum = Number(currKey);
+        storage[currKeyNum-1] = storage[currKey];
+      }
+      delete storage[maxVal];
+    };
+
+    return returnVal;
   };
 
   someInstance.size = function() {
+    let maxVal = findMaxVal(storage);
+    return maxVal;
   };
 
   return someInstance;
