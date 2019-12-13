ea494a52df04a2dbb900691f86151c1e40309d3c
groverpankaj <pgrover2079@gmail.com>
2019-12-12 17:45:36 -0800

    Completes the implementation for tree methods

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,8 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,10 +12,32 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  // create a new tree with value provided and store it in newTree variable
+  var newTree = Tree(value);
+  // push the newTree to this.children array
+  this.children.push(newTree);
 };
 
 treeMethods.contains = function(target) {
+
+  // if this.value matches the target return true
+  if (this.value === target) {
+    return true;
+  }
+  // use a for loop to iterate over this.children
+  for (let i = 0; i < this.children.length; i++) {
+    // call .contains on each child object
+    let child = this.children[i];
+    // if the result of the above call is true then return true
+    if (child.contains(target)) {
+      return true;
+    }
+  }
+  // when we come out of the for in loop return false as default
+  return false;
 };
+
+
 
 
 
