9cb7495da0a5f4c35a055c33b29bfb3c2b964c02
groverpankaj <pgrover2079@gmail.com>
2019-12-13 15:14:24 -0800

    Completes implementation for set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,46 @@
+
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
+// Takes any string and adds it to the Set
 setPrototype.add = function(item) {
+  // check if the item is a string
+  if (typeof(item) === 'string') {
+    // if item has index of === -1 (not found)
+    if (this._storage.indexOf(item) === -1) {
+      // push the item into _storage array
+      this._storage.push(item);
+    }
+  }
 };
 
+// Takes any string and returns a boolean reflecting whether or not it is present
 setPrototype.contains = function(item) {
+  // if item has index of > -1 in _storage array
+  if (this._storage.indexOf(item) > -1) {
+    // return true
+    return true;
+  } else {
+  // else return false
+    return false;
+  }
 };
 
+// Takes any string and removes it from the set, if present
 setPrototype.remove = function(item) {
+  // store the value of the indexof item in posItem variable
+  var posItem = this._storage.indexOf(item);
+  // if posItem > -1 then
+  if (posItem > -1) {
+    // splice the _storage array with argument of posItem
+    this._storage.splice(posItem, 1);
+  }
+
 };
 
 /*
